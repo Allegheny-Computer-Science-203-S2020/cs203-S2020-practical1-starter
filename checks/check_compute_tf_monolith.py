c391f3e06a1a01685bf2ca3aecbbf5d186b75a93
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-01-24 12:11:03 -0500

    Update the check_compute_tf_monolith.

--- checks/check_compute_tf_monolith.py
+++ checks/check_compute_tf_monolith.py
@@ -7,16 +7,15 @@ import sys
 if __name__ == "__main__":
     # assume that all of the checks run correctly and prove otherwise
     exit_code = 0
-    # define the command that will call the check_compute_tf_monolith.py
-    command = "pipenv run python termfrequency/compute_tf_monolith.py inputs/input.txt"
+    # TODO: define the command that will call the check_compute_tf_monolith.py
+    command = ""
     # tokenize this command so that subprocess can accept each of its parts
     tokenized_command = shlex.split(command)
     print("Tokenized command to execute: " + str(tokenized_command))
     print()
     # run the tokenized command and display the output as a byte string
     result = subprocess.run(tokenized_command, stdout=subprocess.PIPE, check=True)
-    print("Output of executed command: " + str(result.stdout))
-    print()
+    # TODO: display the standard output variable inside of the result from the subprocess
     # decode the byte string using UTF-8
     decoded_stdout = str(result.stdout.decode("utf-8"))
     print("Decoded output of executed command: \n" + decoded_stdout)
@@ -25,19 +24,17 @@ if __name__ == "__main__":
     if "live  -  2" not in decoded_stdout:
         print(" - 'live  -  2' is not detected")
         exit_code = 1
-    # check to see if the output contains the first line
-    if "mostly  -  2" not in decoded_stdout:
-        print(" - 'mostly  -  2' is not detected")
-        exit_code = 1
+    # TODO: All all of the required checks for the output's correctness
     # display the final message to indicate checking is finished
+    # NOTE: Follow the previous example for a way to use conditional logic
     print("...Finished checking each line of the output!")
-
     # at least one of the checks failed
     if exit_code == 1:
         print()
-        print("The termfrequency/compute_tf_monolith.py is not working correctly!")
+        # TODO: Add a debugging output to indicate that the program did not check correctly
+        # NOTE: Please refer to the README.md file for more details about expected output
         # indicate that the program should return a non-zero exit code to indicate failure
         sys.exit(exit_code)
     # none of the checks failed
-    print()
-    print("The termfrequency/compute_tf_monolith.py is working correctly!")
+    # TODO: Add a debugging output to indicate that the program did check correctly
+    # NOTE: Please refer to the README.md file for more details about expected output
